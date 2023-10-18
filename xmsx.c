c5e8a5492573e1970f92e430170240d7de4f4725
Dmitrii Lebed <lebed.dmitry@gmail.com>
2023-10-17 22:55:22 -0700

    Simplify xmsx32

--- xmsx.c
+++ xmsx.c
@@ -14,40 +14,28 @@ static uint64_t xmsx32_round(uint64_t h, uint32_t d)
 uint32_t xmsx32(const void *buf, size_t len, uint32_t seed)
 {
     const unsigned char *data = (const unsigned char *)buf;
-    const size_t batch_len = sizeof(uint32_t) * 2;
     uint64_t h = ((uint64_t)seed << 32) | seed;
 
     h = xmsx32_round(h, len);
 
-    while (len >= batch_len) {
-        uint32_t d[2];
-
-        memcpy(d, data, batch_len);
-
-        h = xmsx32_round(h, d[0]);
-        h = xmsx32_round(h, d[1]);
-
-        len -= batch_len;
-        data += batch_len;
-    }
-
     while (len) {
         uint32_t d;
+        const size_t word_size = sizeof(d);
 
         memcpy(&d, data, sizeof(d));
 
-        if (len < 4) {
-            const size_t bits_to_clear = 8 * (sizeof(d) - len);
+        if (len < word_size) {
+            const size_t bits_to_clear = 8 * (word_size - len);
 
             d <<= bits_to_clear;
             d >>= bits_to_clear;
-            len = 4;
+            len = word_size;
         }
 
         h = xmsx32_round(h, d);
 
-        len -= 4;
-        data += 4;
+        len -= word_size;
+        data += word_size;
     }
 
     return xmsx32_round(h, h >> 47);
